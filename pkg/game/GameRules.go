fbf9ba0c71580a95048e4116e100f44e0602c05d
Joost van der Griendt <joostvdg@gmail.com>
2019-08-11 09:59:07 +0200

    should be only two slashes in a comment

--- pkg/game/GameRules.go
+++ pkg/game/GameRules.go
@@ -1,6 +1,6 @@
 package game
 
-/// GameRules the rules for generating this Game's map
+// GameRules the rules for generating this Game's map
 type GameRules struct {
 	MaximumScore         int
 	MinimumScore         int
