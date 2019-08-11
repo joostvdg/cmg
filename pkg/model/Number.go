caf15a6f9381bbd8b7cdcf3c12e4cd5a0c08d62d
Joost van der Griendt <joostvdg@gmail.com>
2019-08-11 10:11:12 +0200

    remove unused field

--- pkg/model/Number.go
+++ pkg/model/Number.go
@@ -1,9 +1,8 @@
 package model
 
-// Number the number fiche that comes on top the Tile, has Name for printing and a Number for calculating
+// Number the number fiche that comes on top the Tile
 // Also has a Score, which is the probability score of the number being rolled with two dices
 type Number struct {
-	Name   string
 	Number int
 	Score  int
 }
