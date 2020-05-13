69758735ef47eb8cfeea6b3af3afabdc3e8f9803
Joost van der Griendt <joostvdg@gmail.com>
2020-05-13 21:33:37 +0200

    fix test

--- pkg/mapgen/map_generator_test.go
+++ pkg/mapgen/map_generator_test.go
@@ -11,7 +11,8 @@ func TestGameCode(t *testing.T) {
 	gameCode := board.GetGameCode(false)
 	expectedLengthOfGameCode := game.NormalGame.TilesCount * 3 // landscape, number, harbor
 	assert.Equal(t, expectedLengthOfGameCode, len(gameCode))
-	inflatedBoard, err := game.InflateNormalGameFromCode(gameCode)
+	normalGameType := game.CreateNormalGame()
+	inflatedBoard, err := game.InflateNormalGameFromCode(gameCode,&normalGameType )
 	assert.NotEmpty(t, inflatedBoard)
 	assert.Empty(t, err)
 }
@@ -21,7 +22,8 @@ func TestGameCodeWithDelimiter(t *testing.T) {
 	gameCode := board.GetGameCode(true)
 	expectedLengthOfGameCode := game.NormalGame.TilesCount*3 + len(board.Board) // landscape, number, harbor + rows
 	assert.Equal(t, expectedLengthOfGameCode, len(gameCode))
-	inflatedBoard, err := game.InflateNormalGameFromCode(gameCode)
+	normalGameType := game.CreateNormalGame()
+	inflatedBoard, err := game.InflateNormalGameFromCode(gameCode, &normalGameType )
 	assert.NotEmpty(t, inflatedBoard)
 	assert.Empty(t, err)
 }
@@ -31,7 +33,8 @@ func TestGameCodeLage(t *testing.T) {
 	gameCode := board.GetGameCode(false)
 	expectedLengthOfGameCode := game.NormalGame.TilesCount * 3 // landscape, number, harbor
 	assert.Equal(t, expectedLengthOfGameCode, len(gameCode))
-	inflatedBoard, err := game.InflateNormalGameFromCode(gameCode)
+	normalGameType := game.CreateNormalGame()
+	inflatedBoard, err := game.InflateNormalGameFromCode(gameCode,&normalGameType )
 	assert.NotEmpty(t, inflatedBoard)
 	assert.Empty(t, err)
 }
@@ -41,7 +44,8 @@ func TestGameCodeWithDelimiterLage(t *testing.T) {
 	gameCode := board.GetGameCode(true)
 	expectedLengthOfGameCode := game.LargeGame.TilesCount*3 + len(board.Board) // landscape, number, harbor + rows
 	assert.Equal(t, expectedLengthOfGameCode, len(gameCode))
-	inflatedBoard, err := game.InflateLargeGameFromCode(gameCode)
+	normalGameType := game.CreateNormalGame()
+	inflatedBoard, err := game.InflateLargeGameFromCode(gameCode,&normalGameType)
 	assert.NotEmpty(t, inflatedBoard)
 	assert.Empty(t, err)
 }
